a75631e13a9754848fb4d56686b788bbf78c69a2
KitSen <loisthen@gmail.com>
2017-11-27 01:13:23 -0500

    fix compilation warning hahaha
    Signed-off-by: KitSen <loisthen@gmail.com>

--- pineapple.c
+++ pineapple.c
@@ -12,10 +12,10 @@ void makePineappleJuice(void)
 {
 
 	printf("Make a pineapple Juice\n");
-	printf("       |_____|        \n");
-	printf("       |     |        \n");
-	printf("       |     |        \n");
-	printf("       |_____|        \n");
+	printf("       |_____|\n");
+	printf("       |     |\n");
+	printf("       |     |\n");
+	printf("       |_____|\n");
 	printf("Done\n");
 }
 
