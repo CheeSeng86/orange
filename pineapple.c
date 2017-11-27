91421cff064d6fc5cd627202115f3e7f18619d13
KitSen <loisthen@gmail.com>
2017-11-27 01:09:44 -0500

    add pineapple juice
    Signed-off-by: KitSen <loisthen@gmail.com>

--- pineapple.c
+++ pineapple.c
@@ -3,7 +3,19 @@
 int main()
 {
 	printf("Hello pineapple\n");
+	makePineappleJuice();
 	return 0;
 
+}
+
+void makePineappleJuice(void)
+{
+
+	printf("Make a pineapple Juice\n");
+	printf("       |_____|        \n");
+	printf("       |     |        \n");
+	printf("       |     |        \n");
+	printf("       |_____|        \n");
+	printf("Done\n");
 }
 
